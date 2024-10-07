dc6747e42a4443877bd020c0483c0aa94ada41e5
ritick <a2katos@mail.ru>
2024-10-07 22:29:37 +0300

    i can upload photo and save on pc

--- go_learn/cmd/app/main.go
+++ go_learn/cmd/app/main.go
@@ -3,5 +3,5 @@ package main
 import "go_learn/cmd/app/go_learn/server"
 
 func main() {
-	server.MainFunc()
+	server.Server()
 }
--- go_learn/server/server.go
+++ go_learn/server/server.go
@@ -2,28 +2,77 @@ package server
 
 import (
 	"fmt"
+	"html/template"
 	"io"
+	"log"
 	"net/http"
 	"os"
-	"text/template"
 )
 
-var tpl *template.Template
+// var tpl *template.Template
 
-// Compile templates on start of the application
-func init() {
-	tpl = template.Must(template.ParseGlob("C:/Users/root/Desktop/photoAtlas/site/templates/*.html"))
+// func init_uploadPage(w http.ResponseWriter, r *http.Request) {
+// 	tpl = template.Must(template.ParseGlob("C:/Users/root/Desktop/photoAtlas/site/templates/upload_image.html"))
+// }
+
+// // Compile templates on start of the application
+// //func init() {
+// //	tpl = template.Must(template.ParseGlob("C:/Users/root/Desktop/photoAtlas/site/templates/home_page.html"))
+// //}
+
+// // Display the named template
+// func display_homePage(w http.ResponseWriter, r *http.Request) {
+// 	tpl.ExecuteTemplate(w, "index", nil)
+
+// }
+// func display_uploadPage(w http.ResponseWriter, r *http.Request) {
+// 	tpl.ExecuteTemplate(w, "index1", nil)
+
+// }
+
+func displayUploadImage(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("OK")
+	t, err := template.ParseFiles("./site/templates/upload_image.html")
+	if err != nil {
+		log.Fatal("PANICCC")
+	}
+	t.ExecuteTemplate(w, "upl", nil)
 }
 
-// Display the named template
-func display(w http.ResponseWriter, r *http.Request) {
-	tpl.ExecuteTemplate(w, "index", nil)
+func displayHomePage(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("./site/templates/home_page.html")
+	if err != nil {
+		log.Fatal("PANICCC")
+	}
+	t.Execute(w, nil)
+}
 
+func handlefunc() {
+	http.HandleFunc("/home", displayHomePage)
+	http.HandleFunc("/upload_image", displayUploadImage)
+	http.HandleFunc("/upload", uploadFile)
+	http.ListenAndServe(":8080", nil)
+}
+
+func Server() {
+	// port := os.Getenv("PORT")
+	// if port == "" {
+	// 	port = "8080"
+	// }
+
+	// mux := http.NewServeMux()
+
+	// mux.HandleFunc("/", Index)
+
+	// http.ListenAndServe(":"+port, mux)
+
+	handlefunc()
 }
 
 func uploadFile(w http.ResponseWriter, r *http.Request) {
 	// Maximum upload of 10 MB files
 	r.ParseMultipartForm(10 << 20)
+	fmt.Println("OK1")
 
 	// Get handler for filename, size and headers
 	file, handler, err := r.FormFile("myFile")
@@ -32,25 +81,26 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(err)
 		return
 	}
-
-	defer file.Close()
-	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
-	fmt.Printf("File Size: %+v\n", handler.Size)
-	fmt.Printf("MIME Header: %+v\n", handler.Header)
-
 	// Create file
+	fmt.Println("OK2")
 	dst, err := os.Create(handler.Filename)
 	defer dst.Close()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	fmt.Println("OK3")
+	defer file.Close()
+	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+	fmt.Printf("File Size: %+v\n", handler.Size)
+	fmt.Printf("MIME Header: %+v\n", handler.Header)
 
 	// Copy the uploaded file to the created file on the filesystem
 	if _, err := io.Copy(dst, file); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	fmt.Println("OK4")
 
 	//fmt.Fprintf(w, "Successfully Uploaded File\n")
 }
@@ -58,21 +108,8 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
 func Index(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
-		display(w, r)
+		displayUploadImage(w, r)
 	case "POST":
 		uploadFile(w, r)
 	}
-}
-
-func MainFunc() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/", Index)
-
-	http.ListenAndServe(":"+port, mux)
 }
